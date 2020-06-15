81614e25c10420d2fbf2be11c883324c919181ba
Arrey Tabe <arrey2391@gmail.com>
2020-06-15 13:25:57 +0100

    added

--- script.js
+++ script.js
@@ -8,8 +8,14 @@ scores = [0, 0];
 activePlayer = 1; //0 is for first player 1 for second player.
 
 
-
 document.querySelector('.dice').style.display = 'none';
+
+//set all scores to zero
+document.getElementById('score-0').innerHTML = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('score-1').innerHTML = '0';
+document.getElementById('current-1').innerHTML = '0';
+
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
     //#1 random number
@@ -22,7 +28,12 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 
 
     //#3 update the roundscore if only the random number is != 1.
-
+    if (dice !== 1) {
+        //add score
+        roundScore += dice;
+    } else {
+        //next player
+    }
 
 })
 
